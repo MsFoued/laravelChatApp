9bb23453e1a53a204fa1120ec627fb6872fe9f8d
Foued <fmoussi@bulldozerinc.com>
2018-06-23 16:20:22 +0100

    #Edit : implement chat feature

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
+
+Route::get('/', 'ChatsController@index');
+Route::get('messages', 'ChatsController@fetchMessages');
+Route::post('messages', 'ChatsController@sendMessage');
 
 Route::get('/home', 'HomeController@index')->name('home');
